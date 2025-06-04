0880c1d29f01dd8e89c9e0c7f7edb213a0a4d29a
96paradise <cnw0806@naver.com>
2025-06-04 16:50:17 +0900

    20250604 404 db

--- korea_sleepTech_404.sql
+++ korea_sleepTech_404.sql
@@ -44,6 +44,7 @@ CREATE TABLE IF NOT EXISTS `teacher` (
     teacher_email VARCHAR(50) UNIQUE NOT NULL,
     teacher_phone_number VARCHAR(20) NOT NULL,
     teacher_subject VARCHAR(50) NOT NULL,
+    teacher_status ENUM('enrolled', 'not_enrolled') DEFAULT 'enrolled', 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (school_id) REFERENCES school(school_id)
@@ -62,8 +63,8 @@ CREATE TABLE IF NOT EXISTS `student` (
     student_phone_number VARCHAR(20) NOT NULL,
     student_birth_date DATE NOT NULL,
     student_affiliation ENUM('liberal_arts', 'natural_sciences') NOT NULL,
-    status ENUM('enrolled', 'not_enrolled', 'graduated') DEFAULT 'enrolled',
-    admission_year YEAR NOT NULL,
+    student_status ENUM('enrolled', 'not_enrolled', 'graduated') DEFAULT 'enrolled',
+    student_admission_year YEAR NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (school_id) REFERENCES school(school_id)
