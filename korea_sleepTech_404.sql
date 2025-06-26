02160a357a284cc102c9b9c2a153b10adc8a0d4b
96paradise <cnw0806@naver.com>
2025-06-26 20:13:10 +0900

    docs/db-update

--- korea_sleepTech_404.sql
+++ korea_sleepTech_404.sql
@@ -19,11 +19,11 @@ CREATE TABLE IF NOT EXISTS email_verification (
 
 -- 학교 승인 대기 테이블
 CREATE TABLE IF NOT EXISTS school_application (
-    school_application_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    school_application_id BIGINT PRIMARY KEY AUTO_INCREMENT,
     school_application_status ENUM('PENDING', 'APPROVED', 'REJECTED') DEFAULT 'PENDING',
-    school_email VARCHAR(30) NOT NULL,
     school_name VARCHAR(30) NOT NULL,
     school_address VARCHAR(255) NOT NULL,
+    school_email VARCHAR(30) NOT NULL,
     school_contact_number VARCHAR(20) NOT NULL,
     school_admin_username VARCHAR(50) UNIQUE NOT NULL,
     school_admin_password VARCHAR(255) NOT NULL,
@@ -63,7 +63,6 @@ CREATE TABLE IF NOT EXISTS school (
 CREATE TABLE IF NOT EXISTS teacher (
     teacher_id VARCHAR(30) PRIMARY KEY,
     school_id BIGINT NOT NULL,
-    subject_id VARCHAR(30) NOT NULL,
     teacher_username VARCHAR(50) UNIQUE NOT NULL,
     teacher_password VARCHAR(255) NOT NULL,
     teacher_name VARCHAR(30) NOT NULL,
@@ -74,8 +73,24 @@ CREATE TABLE IF NOT EXISTS teacher (
     teacher_status ENUM('PENDING', 'APPROVED', 'ON_LEAVE', 'RETIRED') DEFAULT 'PENDING',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (school_id) REFERENCES school(school_id)
+);
+
+-- 과목
+CREATE TABLE IF NOT EXISTS subject (
+    subject_id VARCHAR(30) PRIMARY KEY,
+    school_id BIGINT NOT NULL,
+    teacher_id VARCHAR(30) NOT NULL,
+    subject_name VARCHAR(50) NOT NULL,
+    subject_grade VARCHAR(10) NOT NULL,
+    subject_semester VARCHAR(10) NOT NULL,
+    subject_affiliation ENUM('LIBERAL_ARTS', 'NATURAL_SCIENCES') NOT NULL,
+    subject_status ENUM('APPROVED', 'PENDING', 'REJECTED') DEFAULT 'PENDING',
+    subject_max_enrollment INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (school_id) REFERENCES school(school_id),
-    FOREIGN KEY (subject_id) REFERENCES subject(subject_id)
+    FOREIGN KEY (teacher_id) REFERENCES teacher(teacher_id)
 );
 
 -- 학생
@@ -98,30 +113,13 @@ CREATE TABLE IF NOT EXISTS student (
     FOREIGN KEY (school_id) REFERENCES school(school_id)
 );
 
--- 과목
-CREATE TABLE IF NOT EXISTS subject (
-    subject_id VARCHAR(30) PRIMARY KEY,
-    school_id BIGINT NOT NULL,
-    teacher_id VARCHAR(30) NOT NULL,
-    subject_name VARCHAR(50) NOT NULL,
-    subject_grade VARCHAR(10) NOT NULL,
-    subject_semester VARCHAR(10) NOT NULL,
-    subject_affiliation ENUM('LIBERAL_ARTS', 'NATURAL_SCIENCES') NOT NULL,
-    subject_status ENUM('APPROVED', 'PENDING', 'REJECTED'),
-    subject_max_enrollment INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (school_id) REFERENCES school(school_id),
-    FOREIGN KEY (teacher_id) REFERENCES teacher(teacher_id)
-);
-
 -- 강의
 CREATE TABLE IF NOT EXISTS lecture (
     lecture_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     school_id BIGINT NOT NULL,
     subject_id VARCHAR(30) NOT NULL,
     teacher_id VARCHAR(30) NOT NULL,
-    lecture_day_of_week ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday') NOT NULL,
+    lecture_day_of_week ENUM('MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY') NOT NULL,
     lecture_period INT NOT NULL,
     lecture_allowed_grade VARCHAR(10) NOT NULL,
     lecture_max_enrollment INT NOT NULL,
@@ -150,7 +148,7 @@ CREATE TABLE IF NOT EXISTS course_registration (
 
 -- 수강 이력
 CREATE TABLE IF NOT EXISTS course_history (
-    course_history_id BIGINT PRIMARY KEY,
+    course_history_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     student_id VARCHAR(30),
     lecture_id BIGINT,
     course_history_academic_year YEAR NOT NULL,
@@ -175,5 +173,3 @@ CREATE TABLE IF NOT EXISTS notice (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (school_id) REFERENCES school(school_id)
 );
-
-SELECT school_id FROM school;
